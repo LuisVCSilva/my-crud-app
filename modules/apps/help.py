a7ae071513c1b3875fe98a4044f4f576017ce10c
luis <luisviniciuscostasilva@gmail.com>
2024-12-21 12:04:57 -0300

    help file

--- modules/apps/help.py
+++ modules/apps/help.py
@@ -1,23 +1,38 @@
 import json
-from flask import Blueprint, request,url_for, jsonify
+from flask import Blueprint, request, url_for, jsonify
 import requests
+import os
 
 help = Blueprint('help', __name__)
 
 class Help:
-   def __init__ (self):
-      self.functions = [show_help]
-      self.text = ""
-      
-   @staticmethod
-   def show_help():
-       list_apps_url = url_for('list_apps', _external=True)
-       response = [(item["path"].split("/")[-1],url_for(item["path"].split("/")[-1]+".run")) for item in requests.get(list_apps_url).json() if (item["path"].split("/")[-1])!="help"]
-       print(response)
-       return str(response)
+    def __init__(self):
+        self.functions = [self.show_help]
+        self.text = ""
+    
+    @staticmethod
+    def show_help():
+        # Get a list of Python files (modules) in the current directory
+        modules = [
+            "about.py", "clear.py", "credits.py", "critical_points.py", "derivative.py", 
+            "eigenvalue.py", "eigenvector.py", "get_session.py", "help.py", "integrate.py", 
+            "is_odd.py", "list_files.py", "login.py", "logout.py", "show.py", "signup.py", 
+            "simplify.py", "solve.py", "taylor_series.py", "template.py"
+        ]
 
-   help_method = {'function_name':show_help,'keywords':['help']}
+        # Generate help statements for each module
+        help_statements = []
+        for module in modules:
+            module_name = module.replace(".py", "")
+            help_statement = f"{module_name}<br>"
+            help_statements.append(help_statement)
+        
+        # Return the help statements as a string
+        return "\n".join(help_statements)
+
+    help_method = {'function_name': show_help, 'keywords': ['help']}
       
-@help.route('/apps/help',methods=["GET"])
+@help.route('/apps/help', methods=["GET"])
 def run():
-   return json.dumps({"text":Help.show_help()})   
+    return json.dumps({"text": Help.show_help()})
+
